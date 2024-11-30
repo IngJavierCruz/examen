bb6a0de7edd93b2d3ca538abe1f4480d9b0f436c
Javier Cruz <javier.ortiz@cumplo.com>
2024-11-30 13:53:14 -0600

    Maquetado del grid de los productos e interacción para la solicitud de más registros

--- src/components/SearchScreen/Products/Products.jsx
+++ src/components/SearchScreen/Products/Products.jsx
@@ -1,22 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useGetAccountStatements } from "../../../services/product/getProducts";
 import Product from "../Product/Product";
+import CircularProgress from '@mui/material/CircularProgress';
+
 import * as styles from "./styles.module.scss";
+import { Typography } from "@mui/material";
+
 
 export default function Products() {
-  const { response, getProducts } = useGetAccountStatements();
+  const { response, getProducts, loading } = useGetAccountStatements();
+  const [products, setProducts] = useState([]);
+  const { data, pagination } = response;
+  debugger
+  const [query, setQuery] = useState("computer");
+
+  const onChangePage = (payload) => {
+    const pagination = { ...payload, query };
+    getProducts(pagination);
+  };
 
   useEffect(() => {
-    getProducts({ query: "computer", page: 1 });
+    getProducts({ query, page: 1 });
   }, []);
+
+  useEffect(() => {
+    setProducts((x => [...x, ...response.data]));
+  }, [response]);
 
   return (
     <div className={styles.wrapper}>
+      <Typography>Total registros {products.length}</Typography>
       <div className={styles.products}>
-        {response.map((x, index) => (
+        {products.map((x, index) => (
           <Product key={index} data={x} />
         ))}
       </div>
+
+      {loading && <CircularProgress /> }
+
+      <button onClick={() => onChangePage({ page: pagination.page + 1 })}>Más</button>
     </div>
   );
 }
