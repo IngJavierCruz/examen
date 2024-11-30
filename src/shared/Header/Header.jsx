ccda21328ef1a34dfdd9b39a92343cec250d63de
Javier Cruz <javier.ortiz@cumplo.com>
2024-11-30 11:28:59 -0600

    Icono agregado, nombre en el header agregado

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,12 +1,16 @@
 import Logo from "../../assets/logo.png";
-import { IconButton } from "@mui/material";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { IconButton, Typography } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import * as styles from "./styles.module.scss";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img src={Logo} alt="Logo" className={styles.logo} />
+      <div className={styles.logo}>
+        <img src={Logo} alt="Logo Img" className={styles.img} />
+        <Typography color="textSecondary" className={styles.name} variant="h4">e-Commerce Gapsi</Typography>
+      </div>
+
       <IconButton aria-label="Menu" color="primary">
         <MoreVertIcon fontSize="large" />
       </IconButton>
